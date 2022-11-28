e6640686c4fa991248227aa765dc45be4da85eb7
CameronCrupper <guitardude20795@yahoo.com>
2022-11-28 07:47:31 -0800

    test

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const indexRouter = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const router = require('./routes/index');
 
-app.listen(port, console.log(`Server running on port ${port}`));
-app.use(express.json({ limit: '50mb' }));
-app.use('/', indexRouter);
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+// Parse URL-encoded bodies
+app.use(router);
 
-module.exports = app;
+// Listen to requests
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
