f382ff2f122c13ad6ed20fce613e176bd090f1e4
CameronCrupper <guitardude20795@yahoo.com>
2022-11-28 08:35:27 -0800

    test

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@ const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
 const FilesController = require('../controllers/FilesController');
 
-// Create a new router
 const router = express.Router();
-// GETs
+
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
@@ -15,10 +14,10 @@ router.get('/users/me', UsersController.getMe);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.get('/files/:id/data', FilesController.getFile);
-// POSTs
+
 router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
-// PUTs
+
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 5000;
 const router = require('./routes/index');
 
-// Parse JSON bodies (as sent by API clients)
+const port = process.env.PORT || 5000;
+
 app.use(express.json());
-// Parse URL-encoded bodies
+
 app.use(router);
 
-// Listen to requests
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
